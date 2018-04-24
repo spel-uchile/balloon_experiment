0f3fac70dfee372a6da388ebeed309be6ea73899
matvidal <matias.vidal.v@gmail.com>
2018-04-24 17:18:03 -0300

    Added the program to receive telemetry data from the ballon on Earth Station.

---
+++ sw/Earth_Station/example/earth_station/earth_station.ino
@@ -0,0 +1,42 @@
+/**
+ * @brief Simple Earth Station Example
+ */
+ 
+/*Author: Matías Vidal*/
+
+#include "radio.h"
+
+/*Radio transiver*/
+#define RADIO_SLAVESELECTPIN 2
+#define RADIO_INTERRUPT 3
+#define SDN 4
+
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+
+/*Data store*/
+double dataD[8];
+float dataF[5];
+uint8_t dataU[4];
+
+/*Object Definitions*/
+Radio radio(RADIO_SLAVESELECTPIN, RADIO_INTERRUPT, SDN, SERVER_ADDRESS, CLIENT_ADDRESS);
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // initialize serial communication
+    Serial.begin(115200);
+    // init radio
+    radio.init();
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    radio.read_data(dataD, dataF, dataU);
+}

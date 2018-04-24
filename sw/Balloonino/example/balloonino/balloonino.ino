6632aa7ed2de5563d4e97bb9a6b7559c77b606ee
matvidal <matias.vidal.v@gmail.com>
2018-04-24 10:06:20 -0300

    Implemented GPS library and class, with the functions init and updateData, like the other libraries.

--- sw/Balloonino/example/balloonino/balloonino.ino
+++ sw/Balloonino/example/balloonino/balloonino.ino
@@ -1,67 +1,57 @@
-#include <TinyGPS++.h>
 #include "radio.h"
 #include "imu.h"
 #include "atms.h"
 #include "pines_balloon.h"
+#include "gps.h"
 
 /*Radio transiver*/
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
 
-static const uint32_t GPSBaud = 9600;
-double dataD[3];
+/*GPS baudrate*/
+#define GPS_BAUDRATE 9600
+
+double dataD[8];
 float dataF[5];
+uint8_t dataU[4];
 
 //Create an instance of the objects
 Radio radio(RADIO_SLAVESELECTPIN, RADIO_INTERRUPT, RADIO_SDN, CLIENT_ADDRESS, SERVER_ADDRESS);
 ATMS atms;
 IMU imu(IMU_INTERRUPT, &Serial);
-TinyGPSPlus gps;
+GPS gps;
 
 void setup()
 {
     // Wire.begin();        // Join i2c bus  
     Serial.begin(115200);
     // initialize
-    Serial2.begin(GPSBaud);
     radio.init();
     atms.init();
     imu.init();
+    gps.init();
 }
 
 void loop() {
-    while (Serial2.available() > 0) {
-        if (gps.encode(Serial2.read())) {
-            Serial.print("Latitude: ");
-            Serial.print(gps.location.lat());
-            Serial.print("    Longitude: ");
-            Serial.print(gps.location.lng());
-            Serial.print("      Altitude (m): ");
-            Serial.print(gps.altitude.meters());
-            Serial.print("      Course: ");
-            Serial.print(gps.course.deg());
-            Serial.print("      Speed (m/s): ");
-            Serial.print(gps.speed.mps());
-            Serial.print("      Time: ");
-            Serial.print(gps.time.hour());
-            Serial.print(":");
-            Serial.print(gps.time.minute());
-            Serial.print(":");
-            Serial.print(gps.time.second());
-            Serial.print("      Satellites: ");
-            Serial.println(gps.satellites.value());
-        }
-    }
-    
     atms.updateData();
     imu.updateData();
+    gps.updateData();
     dataD[0] = atms.T;
     dataD[1] = atms.P;
     dataD[2] = atms.a;
+    dataD[3] = gps.lat;
+    dataD[4] = gps.lng;
+    dataD[5] = gps.alt;
+    dataD[6] = gps.crse;
+    dataD[7] = gps.mps;
     dataF[0] = atms.tempC;
     dataF[1] = atms.humidity;
     dataF[2] = imu.ypr[0]*57.29;
     dataF[3] = imu.ypr[1]*57.29;
     dataF[4] = imu.ypr[2]*57.29;
-    radio.send_data(dataD, dataF);
+    dataU[0] = gps.hour;
+    dataU[1] = gps.minute;
+    dataU[2] = gps.second;
+    dataU[3] = gps.sat;
+    radio.send_data(dataD, dataF, dataU);
 }
---
+++ sw/GPS/gps.cpp
@@ -0,0 +1,34 @@
+/**
+ * @brief Simple GPS Library
+ */
+ 
+/*Author: Matias Vidal*/
+
+#include "gps.h"
+
+#define DEBUG_LEVEL 1
+
+GPS::GPS() {
+    gps_baudrate = 9600;
+}
+
+GPS::GPS(int baudrate) {
+    gps_baudrate = baudrate;
+}
+
+void GPS::init() {
+    Serial2.begin(gps_baudrate);
+}
+
+void GPS::updateData() {
+    lat = gps.location.lat();
+    lng = gps.location.lng();
+    alt = gps.altitude.meters();
+    crse = gps.course.deg();
+    mps = gps.speed.mps();
+    hour = gps.time.hour();
+    minute = gps.time.minute();
+    second = gps.time.second();
+    sat = gps.satellites.value();
+}
+

c604c0c5b66251ac9c34b65990618c2701311a30
gdiazh <g.hernan.diaz@gmail.com>
2018-06-17 18:44:28 -0400

    update gps interface

--- sw/bus-arquitecture/gps/gps_com.py
+++ sw/bus-arquitecture/gps/gps_com.py
@@ -2,9 +2,18 @@
 
 __author__ = 'gdiaz'
 
-# GPS COM INTERFACE
+# GPS ZMQ COM INTERFACE
 
-"""Provides a high level interface over ZMQ for gps data exchange.
+"""Provides a high level interface over ZMQ for data exchange.
+"""
+
+"""Description
+    GPS comunication: node1
+
+    console:  send data to node1
+
+    data[gps]-->console[PUB]-> node1
+
 """
 
 import zmq
@@ -12,55 +21,48 @@ import sys
 import time
 
 from threading import Thread
+from time import sleep
 from gps import *
 
 class GpsComInterface:
-    def __init__(self, port="8002", ip="localhost", node=b''):
-        # com parameters
-        self.running = False
-        self.port = port
-        self.ip = ip
-        self.node = node
-        self.data_pub = '/gps_data'
+    def __init__(self):
         # gps arguments
-        self.gps_hd = gps(mode=WATCH_ENABLE) #starting the stream of info
-        # zmq arguments
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUB)
+        self.gps_handler = gps(mode=WATCH_ENABLE) #starting the stream of info
 
-    def initialize(self):
-        # zmq publisher
-        self.socket.bind("tcp://*:%s" % self.port)
+    def test_method(self, cmd):
+        print "rcv from node: "+cmd
 
-    def start(self):
-        # Create subs, services, publishers, threads
-        self.running = True
-        #subscribers
-        self.command_sub = '/gps_cmd'
-        #publishers
-        self.data_pub = '/gps_data'
+    def console(self, port="8003", ip="localhost", origin=10):
+        """ Send messages to node """
+        ctx = zmq.Context(1)
+        sock = ctx.socket(zmq.PUB)
+        # sock.connect('tcp://{}:{}'.format(ip, port))
+        sock.connect('tcp://localhost:8003')
+        node = '3'
+        port = 10
+        print "sending data to node:"+str(node)
 
-        self.pub_th = Thread(target=self.publish_data)
-        self.pub_th.start()
-
-    def stop(self):
-        print "\nKilling Thread..."
-        self.running = False
-        self.pub_th.join()
-
-    def publish_data(self):
-        while self.running:
+        while True:
             try:
                 if True:#TODO: check if data is valid
-                    self.socket.send("%s %4.4f %4.4f %s" % (self.data_pub, self.gps_hd.fix.latitude, self.gps_hd.fix.longitude, str(self.gps_hd.utc)))
-                    print self.gps_hd.fix.latitude, self.gps_hd.fix.longitude, self.gps_hd.utc
-                self.gps_hd.next()
-                # time.sleep(5) #TODO:check this time
-            except KeyboardInterrupt:
-                self.stop()
+                    self.socket.send("%s %4.4f %4.4f %s" % (node, self.gps_handler.fix.latitude, self.gps_handler.fix.longitude, str(self.gps_handler.utc)))
+                    print self.gps_handler.fix.latitude, self.gps_handler.fix.longitude, self.gps_handler.utc
+                self.gps_handler.next()
+            except Exception as e:
+                raise(e)
+            print "send to node: "+str(self.data)
+            # time.sleep(1) #TODO:check this time
 
 if __name__ == '__main__':
     gps = GpsComInterface()
-    gps.initialize()
-    gps.start()
-    gps.publish_data()
+
+    tasks = []
+
+    # Create a console socket
+    console_th = Thread(target=gps.console)
+    # console_th.daemon = True
+    tasks.append(console_th)
+    console_th.start()
+
+    for th in tasks:
+        th.join()
